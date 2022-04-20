d1cac20a288da2d45ada84e955d36635510e70f5
haruluya <2198645565@qq.com>
2022-04-20 10:22:54 +0800

    Update frame work.

---
+++ aerith-ui/src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/ajax',
+    createProxyMiddleware({
+      target: '',
+      changeOrigin: true,
+    })
+  );
+
+};
